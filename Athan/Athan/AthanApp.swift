fc0184e919862e7c5a1772421cc41266971122e7
Khalid Alhazmi <48598891+ipkalid@users.noreply.github.com>
2021-09-11 04:41:03 +0300

    Initial commit

---
+++ Athan/Athan/AthanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthanApp.swift
+//  Athan
+//
+//  Created by Khalid Alhazmi on 11/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AthanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Athan/Athan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Athan
+//
+//  Created by Khalid Alhazmi on 11/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
